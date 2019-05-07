f7aba7357e061a8a76738a39c06b66d8d02bdc7d
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-05-07 22:27:53 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -3,9 +3,16 @@ class Project
   attr_accessor :title 
   attr_reader :backers 
   
+  @@all = [] 
+  
   def initialize(title) 
     @title = title 
     @backers = [] 
+    @@all << self 
+  end 
+  
+  def self.all 
+    @@all 
   end 
   
   def add_backer(name)
